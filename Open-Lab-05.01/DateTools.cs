b14c9a91e15dd1a26a7a2face526d0811d68ebd7
MilosM33 <milosmarejka89@gmail.com>
2020-10-09 19:33:58 +0200

    init

--- Open-Lab-05.01/DateTools.cs
+++ Open-Lab-05.01/DateTools.cs
@@ -1,12 +1,41 @@
 ﻿using System;
-
+using System.Linq;
+using System.Collections.Generic;
 namespace Open_Lab_05._01
 {
     public class DateTools
     {
+        
         public string Century(int year)
         {
-            throw new NotImplementedException();
+            
+            int num = year / 100 + (year% 100 != 0 ? 1 : 0);
+            string temp = num.ToString();
+            char last = temp[temp.Length - 1];
+            if (num < 10 || num > 20)
+            {
+                switch (last)
+                {
+                    case '1':
+                        temp += "st";
+                        break;
+                    case '2':
+                        temp += "nd";
+                        break;
+                    case '3':
+                        temp += "rd";
+                        break;
+
+                    default:
+                        temp += "th";
+                        break;
+                }
+            }
+            else
+                temp += "th";
+            return $"{temp} century";
         }
+
+
     }
 }
